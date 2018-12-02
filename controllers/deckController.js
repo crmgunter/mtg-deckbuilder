ddef7d1920fd5c1b3a98042e82135945ddaf0ecc
crmgunter <crmgunter@gmail.com>
2018-12-02 14:26:50 -0500

    deck controller set up with crud

---
+++ controllers/deckController.js
@@ -0,0 +1,39 @@
+const Deck = require('../models/Deck')
+const User = require('../models/User')
+
+const deckController = {
+    index: (req, res) => {
+        User.findById(req.params.userId).populate('decks')
+        .then((user) => {
+            res.send(user.decks)
+        })
+    },
+    show: (req, res) => {
+        Deck.findById(req.params.deckId)
+        .then(deck => res.send(deck))
+    },
+    create: (req, res) => {
+        User.findById(req.params.userId)
+        .then((user) => {
+            Deck.create(req.body)
+            .then(deck => {
+                user.decks.push(deck)
+                user.save()
+                res.redirect(`/api/users/${req.params.userId}/decks`)
+            })
+        })
+    },
+    update: (req, res) => {
+        Deck.findByIdAndUpdate(req.params.deckId, req.body, {new: true})
+        .then((deck) => res.send(deck))
+    },
+    delete: (req, res) => {
+        Deck.findByIdAndDelete(req.params.deckId)
+        .then(() => {
+            res.send(200)
+            res.redirect(`/api/users/${req.params.userId}/decks`)
+        })
+    }
+}
+
+module.exports = deckController
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const applicationController = require('../controllers/application')
 const userController = require('../controllers/userController')
+const deckController = require('../controllers/deckController')
 
 router.get('/', applicationController.index)
 
@@ -10,5 +11,11 @@ router.get('/users/:userId', userController.show)
 router.post('/users', userController.create)
 router.patch('/users/:userId', userController.update)
 router.delete('/users/:userId', userController.delete)
+
+router.get('/users/:userId/decks', deckController.index)
+router.get('/users/:userId/decks/:deckId', deckController.show)
+router.post('/users/:userId/decks', deckController.create)
+router.patch('/users/:userId/decks/:deckId', deckController.update)
+router.delete('/users/:userId/decks/:deckId', deckController.delete)
 
 module.exports = router
