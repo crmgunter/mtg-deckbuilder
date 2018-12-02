f29a071ea224c3d511e20d2445a75893550bf58a
crmgunter <crmgunter@gmail.com>
2018-12-02 15:37:27 -0500

    heroku push

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1>home</h1>
+                <Link to='/users'>Users</Link>
             </div>
         );
     }
